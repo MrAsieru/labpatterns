46aefdff90197dd8fa9557d7c10c6d4aa92a6708
MrAsieru <asiescp@gmail.com>
2021-10-28 12:18:56 +0200

    SymptomFactory aldatu

--- src/factory/SymptomFactory.java
+++ src/factory/SymptomFactory.java
@@ -1,7 +1,9 @@
 package factory;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import domain.DigestiveSymptom;
 import domain.NeuroMuscularSymptom;
@@ -9,32 +11,33 @@ import domain.RespiratorySymptom;
 import domain.Symptom;
 
 public class SymptomFactory {
-	public SymptomFactory() {}
+	private Map<String, Symptom> symptomList;
+	
+	public SymptomFactory() {
+		this.symptomList = new HashMap<>();
+		symptomList.put("nauseas", new DigestiveSymptom("nauseas", (int)5.0, 1));
+		symptomList.put("vómitos", new DigestiveSymptom("vómitos", (int)4.8, 1));
+		symptomList.put("diarrea", new DigestiveSymptom("diarrea", (int)0.9, 1));
+		
+		symptomList.put("fiebre", new NeuroMuscularSymptom("fiebre", (int)87.9, 5));
+		symptomList.put("astenia", new NeuroMuscularSymptom("astenia", (int)38.1, 5));
+		symptomList.put("cefalea", new NeuroMuscularSymptom("cefalea", (int)13.6, 3));
+		symptomList.put("mialgia", new NeuroMuscularSymptom("mialgia", (int)14.8, 3));
+		symptomList.put("escalofrios", new NeuroMuscularSymptom("escalofrios", (int)11.4, 3));
+		symptomList.put("mareos", new NeuroMuscularSymptom("mareos", (int)3.2, 1));
+		
+		symptomList.put("tos seca", new RespiratorySymptom("tos seca", (int)67.7, 5));
+		symptomList.put("expectoracion", new RespiratorySymptom("expectoracion", (int)33.4, 5));
+		symptomList.put("disnea", new RespiratorySymptom("disnea", (int)18.6, 3));
+		symptomList.put("dolor de garganta", new RespiratorySymptom("dolor de garganta", (int)13.9, 3));
+		symptomList.put("congestión nasal", new RespiratorySymptom("congestión nasal", (int)3.7, 1));
+		symptomList.put("hemoptisis", new RespiratorySymptom("hemoptisis", (int)0.8, 1));
+		symptomList.put("congestion conjuntival", new RespiratorySymptom("congestion conjuntival", (int)2.1, 1));
+		
+	}
 	
 	public Symptom createSymptom(String symptomName) {
-	    List<String> impact5 = Arrays.asList("fiebre", "tos seca", "astenia","expectoracion");
-	    List<Double> index5 = Arrays.asList(87.9, 67.7, 38.1, 33.4);
-	    List<String> impact3 = Arrays.asList("disnea", "dolor de garganta", "cefalea","mialgia","escalofrios");
-	    List<Double> index3 = Arrays.asList(18.6, 13.9, 13.6, 14.8, 11.4);
-	    List<String> impact1 = Arrays.asList("nauseas", "vómitos", "congestión nasal","diarrea","hemoptisis","congestion conjuntival", "mareos");
-	    List<Double> index1 = Arrays.asList(5.0, 4.8, 3.7, 0.9, 0.8, 3.2);
-	    
-	    List<String> digestiveSymptom=Arrays.asList("nauseas", "vómitos","diarrea");
-	    List<String> neuroMuscularSymptom=Arrays.asList("fiebre", "astenia", "cefalea", "mialgia","escalofrios", "mareos");
-	    List<String> respiratorySymptom=Arrays.asList("tos seca","expectoracion","disnea","dolor de garganta", "congestión nasal","hemoptisis","congestion conjuntival");
-
-
-	    int impact=0;
-	    double index=0;
-	    if (impact5.contains(symptomName)) {impact=5; index= index5.get(impact5.indexOf(symptomName));}
-	      else if (impact3.contains(symptomName)) {impact=3;index= index3.get(impact3.indexOf(symptomName));}
-	        else if (impact1.contains(symptomName)) {impact=1; index= index1.get(impact1.indexOf(symptomName));}
-	 
-	    if (impact!=0)  {
-	    	if (digestiveSymptom.contains(symptomName)) return new DigestiveSymptom(symptomName,(int)index, impact);
-	    	if (neuroMuscularSymptom.contains(symptomName)) return new NeuroMuscularSymptom(symptomName,(int)index, impact);
-	    	if (respiratorySymptom.contains(symptomName)) return new RespiratorySymptom(symptomName,(int)index, impact);
-	    }
-	    return null;
+		if (symptomList.containsKey(symptomName)) return symptomList.get(symptomName);
+		else return null;
 	}
 }

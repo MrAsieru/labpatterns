4367a32f78f3b9e5bc8b7e5b3cced2fbbef96ba2
Mikel Laorden <laordenmikel@gmail.com>
2021-10-29 10:08:20 +0200

    Observable patroiaren inplementazioa Simplefactory patroiaren implementazioari egokitu

--- src/observer/Main1Prot.java
+++ src/observer/Main1Prot.java
@@ -3,6 +3,7 @@ package observer;
 import java.util.Observable;
 
 import domain.Covid19Pacient;
+import factory.SymptomFactory;
 
 public class Main1Prot {
 
@@ -10,14 +11,14 @@ public class Main1Prot {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-	
-		Observable pacient=new Covid19Pacient("aitor", 35);
+		SymptomFactory sf = new SymptomFactory();
+		Observable pacient=new Covid19Pacient("aitor", 35, sf);
 		PacientObserverGUI pacientGUI= new PacientObserverGUI (pacient);
 		
 		PacientSymptomGUI frame = new PacientSymptomGUI((Covid19Pacient) pacient);
 
 		// 2. Covid Pacient
-		Observable pacient2=new Covid19Pacient("pedro", 23);
+		Observable pacient2=new Covid19Pacient("pedro", 23, sf);
 		PacientObserverGUI pacientGUI2= new PacientObserverGUI (pacient2);
 		
 		PacientSymptomGUI frame2 = new PacientSymptomGUI((Covid19Pacient) pacient2);
--- src/observer/Main2Prot.java
+++ src/observer/Main2Prot.java
@@ -1,43 +1,45 @@
 package observer;
 
 import domain.Covid19Pacient;
+import factory.SymptomFactory;
 
 public class Main2Prot {
+	private static SymptomFactory sf = new SymptomFactory();
 
 	public static void main(String[] args) {
-		//lehenAriketa();
-		//bigarrenAriketa();
+		lehenAriketa();
+		bigarrenAriketa();
 		hirugarrenAriketa();
 		
 	}
-	private static void hirugarrenAriketa() {
-		// 3. ariketa
-		Covid19Pacient cp = new Covid19Pacient("aitor", 36);
-		sortu3GUISetForPacient(cp);
-		SemaphorGUI sg = new SemaphorGUI(cp);
-		
-	}
+
 	private static void lehenAriketa(){
 		// 1. ariketa
-		Covid19Pacient cp = new Covid19Pacient("aitor", 36);
+		Covid19Pacient cp = new Covid19Pacient("aitor", 36, sf);
 		sortu3GUISetForPacient(cp);
 	}
 	
 	private static void bigarrenAriketa() {
 		// 2. ariketa
-
-		Covid19Pacient cp1 = new Covid19Pacient("pepe", 56);
+		Covid19Pacient cp1 = new Covid19Pacient("pepe", 56, sf);
 		sortu3GUISetForPacient(cp1);
-		Covid19Pacient cp2 = new Covid19Pacient("ane", 36);
+		Covid19Pacient cp2 = new Covid19Pacient("ane", 36, sf);
 		sortu3GUISetForPacient(cp2);
-		Covid19Pacient cp3 = new Covid19Pacient("sara", 19);
+		Covid19Pacient cp3 = new Covid19Pacient("sara", 19, sf);
 		sortu3GUISetForPacient(cp3);
+	}
+	private static void hirugarrenAriketa() {
+		// 3. ariketa
+		Covid19Pacient cp = new Covid19Pacient("aitor", 36, sf);
+		sortu3GUISetForPacient(cp);
+		new SemaphorGUI(cp);
+		
 	}
 	
 	private static void sortu3GUISetForPacient(Covid19Pacient cp) {
-		PacientSymptomGUI psg = new PacientSymptomGUI(cp);
-		PacientObserverGUI pog = new PacientObserverGUI(cp);
-		PacientThermometerGUI ptg = new PacientThermometerGUI(cp);
+		new PacientSymptomGUI(cp);
+		new PacientObserverGUI(cp);
+		new PacientThermometerGUI(cp);
 	}
 	
 	
